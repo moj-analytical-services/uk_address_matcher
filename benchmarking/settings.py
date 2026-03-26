96c22ec19df83658ec15522c47ed207bda3d45f6
ThomasHepworth <thomas.hepworth@justice.gov.uk>
2026-03-26 15:06:41 +0000

    feat: add residential + commercial filter for canonical

--- benchmarking/settings.py
+++ benchmarking/settings.py
@@ -8,4 +8,7 @@ SAMPLE_MODE = False
 CANONICAL_PATH = get_env_setting("UKAM_OS_CANONICAL_PREPARED")
 
 APPLY_CANONICAL_FILTER = False
-CANONICAL_FILTER_SQL: str | None = None
+# CANONICAL_FILTER_SQL: str | None = None
+CANONICAL_FILTER_SQL = (
+    "substr(classificationcode, 1, 1) = 'R' or substr(classificationcode, 2, 1) = 'C'"
+)
