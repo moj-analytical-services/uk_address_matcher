49d820878096331dd0abc90cb4623372c021d61a
ThomasHepworth <thomas.hepworth@justice.gov.uk>
2026-07-30 16:05:16 +0100

    feat(labelling): add first page of labelling tool
    Includes an init build of the labelling tool, which includes the overview tab, which gives a tabulated view of the labelling env.
    
    The init app has a 10 minute timeout and displays a basic javascript web interface for users to click around.

--- uk_address_matcher/labelling/__init__.py
+++ uk_address_matcher/labelling/__init__.py
@@ -1,3 +1,4 @@
 from .bundle import export_labelling_bundle
+from .server import launch_labelling_app
 
-__all__ = ["export_labelling_bundle"]
+__all__ = ["export_labelling_bundle", "launch_labelling_app"]
