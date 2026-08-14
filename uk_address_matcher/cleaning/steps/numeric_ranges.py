6663b85dde48ee99ae01b8a434e8d4c14e561024
ThomasHepworth <thomas.hepworth@justice.gov.uk>
2026-08-14 23:28:16 +0100

    refactor: simplify numeric range extraction logic in SQL pipeline

--- uk_address_matcher/cleaning/steps/numeric_ranges.py
+++ uk_address_matcher/cleaning/steps/numeric_ranges.py
@@ -16,115 +16,104 @@ def _derive_numeric_range(
     WITH classified_tokens AS (
         SELECT
             input.*,
-            list_filter(
-                regexp_extract_all(
-                    clean_full_address,
-                    '\\b\\d{{1,5}}[A-Z]?-\\d{{1,5}}[A-Z]?\\b'
-                ),
-                token -> TRUE
-            ) AS range_tokens,
-            CASE
-                WHEN len(regexp_extract_all(
-                    clean_full_address,
-                    '\\b\\d{{1,5}}[A-Z]?-\\d{{1,5}}[A-Z]?\\b'
-                )) = 0
-                    THEN list_filter(
-                        numeric_tokens,
-                        token -> regexp_matches(token, '^\\d{{1,5}}[A-Z]?$')
-                    )
-                ELSE []::VARCHAR[]
-            END AS scalar_tokens
+            regexp_extract_all(
+                clean_full_address,
+                '\\b\\d{{1,5}}[A-Z]?-\\d{{1,5}}[A-Z]?\\b'
+            ) AS range_tokens
         FROM {{input}} AS input
-    )
-    SELECT
-        * EXCLUDE (range_tokens, scalar_tokens),
-        len(range_tokens)::UTINYINT AS numeric_range_count,
-        list_transform(
-            range_tokens,
-            token -> struct_pack(
-                raw := token,
-                lower := TRY_CAST(
-                    regexp_extract(token, '^(\\d{{1,5}})', 1) AS UINTEGER
-                ),
-                upper := TRY_CAST(
-                    regexp_extract(token, '-(\\d{{1,5}})', 1) AS UINTEGER
-                ),
-                width := GREATEST(
-                    0,
-                    TRY_CAST(regexp_extract(
-                        token, '-(\\d{{1,5}})', 1
-                    ) AS BIGINT)
-                    - TRY_CAST(regexp_extract(
-                        token, '^(\\d{{1,5}})', 1
-                    ) AS BIGINT)
-                )::UINTEGER,
-                lower_suffix := NULLIF(
-                    regexp_extract(token, '^(\\d{{1,5}})([A-Z])?-', 2),
-                    ''
-                ),
-                upper_suffix := NULLIF(
-                    regexp_extract(token, '-\\d{{1,5}}([A-Z])?$', 1),
-                    ''
-                ),
-                role := CASE
-                    WHEN regexp_matches(clean_full_address, '\\b(REF|REFERENCE)\\b')
-                        THEN 3::UTINYINT
-                    ELSE 1::UTINYINT
-                END,
-                flags := (
-                    CASE
-                        WHEN TRY_CAST(regexp_extract(
-                            token, '^(\\d{{1,5}})', 1
-                        ) AS UINTEGER)
-                            > TRY_CAST(regexp_extract(
-                                token, '-(\\d{{1,5}})', 1
-                            ) AS UINTEGER)
-                        THEN 1 ELSE 0
-                    END
-                    + CASE
-                        WHEN TRY_CAST(regexp_extract(
-                            token, '^(\\d{{1,5}})', 1
-                        ) AS UINTEGER)
-                            = TRY_CAST(regexp_extract(
-                                token, '-(\\d{{1,5}})', 1
-                            ) AS UINTEGER)
-                        THEN 2 ELSE 0
-                    END
-                    + CASE
-                        WHEN regexp_matches(token, '^\\d{{1,5}}[A-Z]-')
-                            OR regexp_matches(token, '-\\d{{1,5}}[A-Z]$')
-                        THEN 4 ELSE 0
-                    END
-                    + CASE
-                        WHEN TRY_CAST(regexp_extract(
+    ),
+    range_attributes AS (
+        SELECT
+            classified_tokens.*,
+            list_transform(
+                range_tokens,
+                token -> struct_pack(
+                    raw := token,
+                    lower := TRY_CAST(
+                        regexp_extract(token, '^(\\d{{1,5}})', 1) AS UINTEGER
+                    ),
+                    upper := TRY_CAST(
+                        regexp_extract(token, '-(\\d{{1,5}})', 1) AS UINTEGER
+                    ),
+                    width := GREATEST(
+                        0,
+                        TRY_CAST(regexp_extract(
                             token, '-(\\d{{1,5}})', 1
                         ) AS BIGINT)
-                            - TRY_CAST(regexp_extract(
-                                token, '^(\\d{{1,5}})', 1
-                            ) AS BIGINT) > {maximum_width}
-                        THEN 8 ELSE 0
-                    END
-                    + CASE
+                        - TRY_CAST(regexp_extract(
+                            token, '^(\\d{{1,5}})', 1
+                        ) AS BIGINT)
+                    )::UINTEGER,
+                    lower_suffix := NULLIF(
+                        regexp_extract(token, '^(\\d{{1,5}})([A-Z])?-', 2),
+                        ''
+                    ),
+                    upper_suffix := NULLIF(
+                        regexp_extract(token, '-\\d{{1,5}}([A-Z])?$', 1),
+                        ''
+                    ),
+                    role := CASE
                         WHEN regexp_matches(clean_full_address, '\\b(REF|REFERENCE)\\b')
-                        THEN 16 ELSE 0
-                    END
-                )::UTINYINT,
-                lower_tf := NULL::DOUBLE
+                            THEN 3::UTINYINT
+                        ELSE 1::UTINYINT
+                    END,
+                    flags := (
+                        CASE
+                            WHEN TRY_CAST(regexp_extract(
+                                token, '^(\\d{{1,5}})', 1
+                            ) AS UINTEGER)
+                                > TRY_CAST(regexp_extract(
+                                    token, '-(\\d{{1,5}})', 1
+                                ) AS UINTEGER)
+                            THEN 1 ELSE 0
+                        END
+                        + CASE
+                            WHEN TRY_CAST(regexp_extract(
+                                token, '^(\\d{{1,5}})', 1
+                            ) AS UINTEGER)
+                                = TRY_CAST(regexp_extract(
+                                    token, '-(\\d{{1,5}})', 1
+                                ) AS UINTEGER)
+                            THEN 2 ELSE 0
+                        END
+                        + CASE
+                            WHEN regexp_matches(token, '^\\d{{1,5}}[A-Z]-')
+                                OR regexp_matches(token, '-\\d{{1,5}}[A-Z]$')
+                            THEN 4 ELSE 0
+                        END
+                        + CASE
+                            WHEN TRY_CAST(regexp_extract(
+                                token, '-(\\d{{1,5}})', 1
+                            ) AS BIGINT)
+                                - TRY_CAST(regexp_extract(
+                                    token, '^(\\d{{1,5}})', 1
+                                ) AS BIGINT) > {maximum_width}
+                            THEN 8 ELSE 0
+                        END
+                        + CASE
+                            WHEN regexp_matches(clean_full_address, '\\b(REF|REFERENCE)\\b')
+                            THEN 16 ELSE 0
+                        END
+                    )::UTINYINT,
+                    lower_tf := NULL::DOUBLE
+                )
+            ) AS parsed_range_attributes
+        FROM classified_tokens
+    )
+    SELECT
+        * EXCLUDE (range_tokens, parsed_range_attributes),
+        CASE
+            WHEN len(list_filter(
+                parsed_range_attributes,
+                attribute -> (attribute.flags & 27) = 0
+            )) > 0
+            THEN list_filter(
+                parsed_range_attributes,
+                attribute -> (attribute.flags & 27) = 0
             )
-        ) AS numeric_range_attributes,
-        list_transform(
-            scalar_tokens,
-            token -> TRY_CAST(regexp_extract(token, '^\\d{{1,5}}', 0) AS UINTEGER)
-        ) AS numeric_scalar_tokens,
-        list_transform(
-            scalar_tokens,
-            token -> NULLIF(regexp_extract(token, '[A-Z]$', 0), '')
-        ) AS numeric_scalar_suffixes,
-        list_transform(
-            scalar_tokens,
-            token -> 0::UTINYINT
-        ) AS numeric_scalar_roles
-    FROM classified_tokens
+            ELSE NULL
+        END AS numeric_range_attributes
+    FROM range_attributes
     """
 
 
