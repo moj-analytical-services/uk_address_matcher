0af391f86e67a69241002fd215274d04f13358cf
Robin Linacre <robinlinacre@hotmail.com>
2026-09-11 13:29:39 +0100

    lint

--- benchmarking/insights/run_persistence.py
+++ benchmarking/insights/run_persistence.py
@@ -96,7 +96,8 @@ def _vega_lite_html(*, title: str, chart_definition: dict[str, Any]) -> str:
 <body>
     <div class=\"card\">
         <h1>{title}</h1>
-        <p>Overlayed precision-recall curves with recall-aligned reductions in false positives.</p>
+        <p>Overlayed precision-recall curves with recall-aligned reductions in
+            false positives.</p>
         <div id=\"chart\"></div>
     </div>
     <script>
